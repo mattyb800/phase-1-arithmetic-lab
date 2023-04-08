b75ce50ad852c380ae989d92521e743d290307d9
Matt Brouwer <notjonbonjovi@gmail.com>
2023-04-08 17:38:49 -0400

    arithmetic test

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
+let num1 = 2
+let num2 = 31
 // Write your code here
+let multiply = 2 * 31
+
+
+let random = Math.floor(Math.random() +1);
+let num3 = 19
+let num4 = 5
+
+let mod = 19 % 5;
+
+let max = Math.max(2,3,8,20)
